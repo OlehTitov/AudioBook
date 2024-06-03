36dc82ec5a9d181f8c751dbaebe954f6ae8b0cb4
Oleh Titov <oleg.titov81@gmail.com>
2024-06-03 15:27:04 +0200

    refactor BookSummaryView to separate file

---
+++ AudioBook/BookSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  BookSummaryView.swift
+//  AudioBook
+//
+//  Created by Oleh Titov on 03.06.2024.
+//
+
+import SwiftUI
+
+struct BookSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookSummaryView()
+}
